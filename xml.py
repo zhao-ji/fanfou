0f6bba0d2f15a3b719dcb3ea6a79bc76a02b3e53
zhao-ji <iamfrommop@gmail.com>
2014-01-11 01:47:46 -0500

    clean

--- xml.py
+++ xml.py
@@ -12,25 +12,11 @@ sys.setdefaultencoding('utf-8')
 class xmlmsg:
     def GET(self):
         for i in range (703,0,-1):
-            f   = file('/home/nightwish/Music/fanfou/treeholes/'+str(i)+'.xml')
+            f   = file('/var/www/fanfou/treeholes/'+str(i)+'.xml')
             xml = f.read()
             f.close()
-            try:
-                xml = etree.fromstring(xml)
-            except Exception,e:
-                m   = file('/home/nightwish/Music/fanfou/error.txt','a')
-                s = '文件：%d.xml  :%s \n' %(i,e)
-                m.write(s)
-                m.close()
-                continue
+            xml = etree.fromstring(xml)
             for j in range(len(xml)-1,-1,-1):
-                try:
-                    talk = xml[j][0].text.encode('utf-8')
-                except Exception,e:
-                    m   = file('/home/nightwish/Music/fanfou/error.txt','a')
-                    s = '文件：%d.xml  第%d行   :%s \n' %(i,j,e)
-                    m.write(s)
-                    m.close()
-                    continue
+                talk = xml[j][0].text.encode('utf-8')
                 message.save(talk,0)
             
