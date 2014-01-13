0a63d85e8e49901f58b4ea8cc668b47675f4da9b
zhao-ji <iamfrommop@gmail.com>
2014-01-13 08:30:33 -0500

    wechat got!

--- url.py
+++ url.py
@@ -1,16 +1,8 @@
 #!/usr/bin.env python
 #-*- coding=utf-8 -*-
-'''  神秘肉洞 url 结构  '''
-from xml    import xmlmsg
-import wechatmsg
 
-<<<<<<< HEAD
-urls  = ('/wechat',wechatmsg,
-       '/dm_fetch',directmsg,
-#       '/xml_fetch',xmlmsg,
-       '/text_sent',sendtext,
-       '/photo_sent',sendphoto)
-=======
-urls  = ('/wechat',wechatmsg
-       #'/xml_fetch',xmlmsg)
->>>>>>> 032a3bc7d02dc616da2351673148583bf3caf133
+#from xml    import xmlmsg
+from wechat import wechatmsg
+
+urls  = ('/','wechatmsg')
+       #'/xml_fetch',xmlmsg
