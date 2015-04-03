47a2bffda4265f2c217beb22a2b818873686d587
ramnode <i@minganci.org>
2015-04-03 22:28:28 +0800

    fixbug: a little import

--- code.py
+++ code.py
@@ -5,10 +5,12 @@
 import web
 import time
 
+from wechat import wechatmsg
+
 urls  = ('/',wechatmsg)
 
 if __name__ == "__main__":
-    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+    # web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app = web.application(urls, globals())
     time.sleep(0.01)
     app.run()
--- fanfou.py
+++ fanfou.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 #-*- coding=utf-8 -*-
 
-import string
+from os.path import join, dirname, abspath, exists
 import time
 
 from lxml import etree
+import logbook
 
 import api
 
@@ -17,8 +18,10 @@ def directmsg():
             for i in range(num):
                 id = xml[i][0].text
                 msg = xml[i][1].text
-                sendtext(content)
+                logbook.info(msg.encode("utf8"))
+                sendtext(msg.encode("utf8"))
                 code = api.post('direct_messages/destroy', id=id)
+                logbook.info("destory")
                 while code != 1:
                     code, xml = api.fanfou('direct_messages/destroy',{'id':id})
 
@@ -26,11 +29,23 @@ def sendtext(content):
     xml = api.get('account/rate_limit_status')
     xml = etree.fromstring(xml)
     limit_num = xml[1].text
-    if string.atoi(limit_num) == 0:
+    if int(limit_num) == 0:
         return
     code = api.post('statuses/update',status=content)
+    logbook.info("sent!")
 
 if __name__ == '__main__':
+    server_log_file = join(dirname(abspath(__file__)), "log/wechat.log")
+    if not exists(server_log_file):
+        open(server_log_file, "w").close()
+
+    local_log = logbook.FileHandler(server_log_file)
+    local_log.format_string = (
+        u'[{record.time:%H:%M:%S}] '
+        u'lineno:{record.lineno} '
+        u'{record.level_name}:{record.message}')
+    local_log.push_application()
+
     while True:
         directmsg()
         time.sleep(60)
